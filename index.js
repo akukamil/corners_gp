5750f55c000f8d97e102b1d1399d7030871214ae
akukamil <chamilioner_kid@mail.ru>
2023-09-29 16:44:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4780,7 +4780,8 @@ auth1 = {
 			my_data.uid 	= _player.getUniqueID().replace(/\//g, "Z");
 			my_data.pic_url = _player.getPhoto('medium');						
 			my_data.name = my_data.name || this.get_random_name(my_data.uid);
-			
+			if (my_data.pic_url === 'https://games-sdk.yandex.ru/games/api/sdk/v1/player/avatar/0/islands-retina-medium')
+				my_data.pic_url = 'https://api.dicebear.com/7.x/adventurer/svg?seed='+my_data.uid;	
 			return;
 		}
 		
