45cc8b4147daa7f562eb04a68e399465c76727fd
akukamil <chamilioner_kid@mail.ru>
2024-01-28 19:18:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3440,6 +3440,8 @@ chat = {
 	drag_sy:-999,	
 	recent_msg:[],
 	moderation_mode:0,
+	block_next_click:0,
+	kill_next_click:0,
 	
 	activate() {		
 
@@ -3544,14 +3546,31 @@ chat = {
 		
 		if (this.moderation_mode){
 			console.log(player_data.index,player_data.uid,player_data.name.text,player_data.msg.text);
-			return
+			fbs_once('players/'+player_data.uid+'/games').then((data)=>{
+				console.log('сыграно игр: ',data)
+			})
+			return;
 		}
 		
-		if (objects.feedback_cont.visible){			
-			feedback.response_message(player_data.uid,player_data.name.text);			
-		}else{			
-			lobby.show_invite_dialog_from_chat(player_data.uid,player_data.name.text);			
+		if (this.block_next_click){			
+			fbs.ref('blocked/'+player_data.uid).set(Date.now())
+			console.log('Игрок заблокирован: ',player_data.uid);
+			this.block_next_click=0;
+			return;
 		}
+		
+		if (this.kill_next_click){			
+			fbs.ref('inbox/'+player_data.uid).set({message:'CLIEND_ID',tm:Date.now(),client_id:999999});
+			console.log('Игрок убит: ',player_data.uid);
+			this.kill_next_click=0;
+			return;
+		}
+		
+		if (objects.feedback_cont.visible)
+			feedback.response_message(player_data.uid,player_data.name.text);			
+		else		
+			lobby.show_invite_dialog_from_chat(player_data.uid,player_data.name.text);			
+
 		
 		
 	},
