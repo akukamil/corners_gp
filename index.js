6416c0c1f30b91bef5f137b8bb7a9abd02670d77
akukamil <chamilioner_kid@mail.ru>
2025-01-25 15:30:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3758,7 +3758,7 @@ chat={
 
 		anim2.add(objects.chat_cont,{alpha:[0, 1]}, true, 0.1,'linear');
 		//objects.bcg.texture=assets.lobby_bcg;
-		objects.chat_enter_button.visible=true;//my_data.games>=this.games_to_chat;
+		objects.chat_enter_button.visible=my_data.games>=this.games_to_chat;
 		
 		if(my_data.blocked)		
 			objects.chat_enter_button.texture=assets.chat_blocked_img;
